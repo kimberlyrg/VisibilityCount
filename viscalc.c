17e711ed775a9477c12ea1e0ce4535853024bf25
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-06-19 16:39:22 +0530

    Use array instead of linkedlist

--- viscalc.c
+++ viscalc.c
@@ -1,82 +1,92 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 
 typedef struct Bar {
 	double xValue;
 	double yValue;
 	int count;
-	struct Bar *next;
 } Bar;
 
-void calculateVisibility(Bar *start){
-	Bar *present = start;
-	while(present!=NULL){
-		Bar *target = present->next;
-		double ta = present->xValue;
-		double ya = present->yValue;
+void printBar(Bar b, int i){
+	printf("\nBar %d xValue = %g yValue = %g count = %d", i, b.xValue, b.yValue, b.count);
+}
+
+void calculateVisibility(Bar *start, int count){
+	int i = 0;
+	while(i<count){
+		Bar present = start[i];
+//		printBar(present, i);
+		int j = i+1;
+
+		double ta = present.xValue;
+		double ya = present.yValue;
 		
-		double tc = present->xValue;
-		double yc = present->yValue;
-		while(target!=NULL){
-			if(target==present->next){
-				present->count++;
-				target->count++;
-				tc = target->xValue;
-				yc = target->yValue;
+		double tc = present.xValue;
+		double yc = present.yValue;
+		while(j<count){
+			Bar target = start[j];
+//			printBar(target, j);
+			if(j==i+1){
+				start[i].count++;	
+				start[j].count++;
+
+				tc = target.xValue;
+				yc = target.yValue;
 			}
 			else{
-				double tb = target->xValue;
-				double yb = target->yValue;
+				double tb = target.xValue;
+				double yb = target.yValue;
 				double div = ((ya - yb) * ((tb - tc) / (tb - ta))) + yb;
 
 				if(yc < div){
-					present->count++;
-					target->count++;
+					start[i].count++;
+					start[j].count++;
 
 					if(yb > yc){
-						tc = target->xValue;
-						yc = target->yValue;
+						tc = target.xValue;
+						yc = target.yValue;
 					}
 						
 				}
 			}
-			target = target->next;
+			j++;
 		}
-		present = present->next;
+				
+//		printf("\nPresent = %d", present.count);
+		i++;
 	}
 }
 
-void readFromFile(Bar **start, char *fileName){
+typedef struct Value{
+	char *string;
+	struct Value *nextValue;
+} Value;
+
+void readFromFile(Bar **start, int *count, char *fileName){
 	FILE *f = fopen(fileName, "rb");
 	if(f==NULL){
 		printf("\nError : Unable to open input file %s!\n", fileName);
 		exit(1);
 	}
 	double temp;
-	Bar *tempbar, *prevbar = NULL;
-	double c = 1;
+	long c = 0;
+	Value *head = NULL, *prev = NULL;
 	char line[256];
 	printf("\nParsing file..");
 	while(fgets(line, sizeof(line), f)){
-		temp = atof(line);
-		tempbar = (Bar *)malloc(sizeof(Bar));
-		if(tempbar==NULL){
-			printf("\nError : Insufficient memory!\n");
-			fclose(f);
-			exit(2);
+//		printf("\nLine : %s Head : %p Prev : %p", line, head, prev);
+		Value *now = (Value *)malloc(sizeof(Value));
+		now->nextValue = NULL;
+		now->string = strdup(line);
+		if(!head){
+			head = now;
 		}
-		tempbar->xValue = c;
-		tempbar->yValue = temp;
-		tempbar->count = 0;
-		tempbar->next = NULL;
-
-		if(prevbar)
-			prevbar->next = tempbar;
-		else
-			(*start) = tempbar;
-
-		prevbar = tempbar;
+		else{
+			prev->nextValue = now;
+		}
+		prev = now;
 		c++;
 	}
 	if(!feof(f)){
@@ -85,20 +95,38 @@ void readFromFile(Bar **start, char *fileName){
 		exit(3);
 	}
 	fclose(f);
-	printf("\nTotal %.0f values read..", --c);
+	
+	(*start) = (Bar *)malloc(sizeof(Bar)*c);
+	Bar *bars = (*start);
+	int i = 0;
+	Value *cur = head;
+	while(i<c){
+		temp = atof(cur->string);
+//		printf("\nValue : %g", temp);
+		bars[i].xValue = i+1;
+		bars[i].yValue = temp;
+		bars[i].count = 0;
+		if(cur->nextValue!=NULL)
+			cur = cur->nextValue;
+		i++;
+	}
+
+	printf("\nTotal %ld values read..", c);
+	(*start) = bars;
+	*count = c;
 }
 
-void writeToFile(Bar *start, char *fileName){
+void writeToFile(Bar *start, int count, char *fileName){
 	FILE *f = fopen(fileName, "w");
 	if(f==NULL){
 		printf("\nError : Unable to create file!\n");
 		exit(1);
 	}
-	int count = 1;
-	while(start!=NULL){
-		fprintf(f, "Bar : %3d\tValue : %12.6f\tNodes : %3d\n", count, start->yValue, start->count);
-		start = start->next;
-		count++;
+	int c = 0;
+	while(c<count){
+		Bar present = start[c];
+		fprintf(f, "Bar : %3d\tValue : %12.6f\tNodes : %3d\n", (c+1), present.yValue, present.count);
+		c++;
 	}
 	fclose(f);
 }
@@ -118,20 +146,21 @@ int main(int argc, char *argv[]){
 	setbuf(stdout, NULL);
 	printf("\nReading file..");
 	Bar *start;
+	int count;
 	timer = clock();
-	readFromFile(&start, argv[1]);
+	readFromFile(&start,&count, argv[1]);
 	seconds = ((double) (clock() - timer)) / CLOCKS_PER_SEC;
 	printf("\nReading took %g seconds..", seconds);
 
 	printf("\nCalculating visibility..");
 	timer = clock();
-	calculateVisibility(start);
+	calculateVisibility(start, count);
 	seconds = ((double) (clock() - timer)) / CLOCKS_PER_SEC;
 	printf("\nCalculation took %g seconds..", seconds);
 
 	printf("\nWriting out to file..");
 	timer = clock();
-	writeToFile(start, argv[2]);
+	writeToFile(start, count, argv[2]);
 	seconds = ((double) (clock() - timer)) / CLOCKS_PER_SEC;
 	printf("\nWriting took %g seconds..", seconds);
 
