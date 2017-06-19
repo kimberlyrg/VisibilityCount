0300749234178998a4d9dde50878c639447d7a66
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-06-19 17:32:56 +0530

    Added random input generation and free functions

--- viscalc.c
+++ viscalc.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 
 typedef struct Bar {
 	double xValue;
@@ -107,13 +108,28 @@ void printUsage(){
 	printf("\nUsage : viscalc input_file output_file\n");
 }
 
+void freeBars(Bar *head){
+	while(head!=NULL){
+		Bar *backup = head;
+		head = head->next;
+		free(backup);
+	}
+}
+
+void generateRandomAndTest(int);
+
 int main(int argc, char *argv[]){
 	clock_t timer;
 	double seconds;
-	if(argc!=3){
+	if((argc!=3 && argc!=4) || (argc==4 && strcmp(argv[3], "randomtest")!=0) ){
 		printf("\nError : Wrong number of inputs!");
 		printUsage();
 		return 1;
+	}
+	if(strcmp(argv[1], "test")==0){
+		printf("\nGenerating %d random input values..", atoi(argv[2]));
+		generateRandomAndTest(atoi(argv[2]));
+		return 0;
 	}
 	setbuf(stdout, NULL);
 	printf("\nReading file..");
@@ -135,5 +151,41 @@ int main(int argc, char *argv[]){
 	seconds = ((double) (clock() - timer)) / CLOCKS_PER_SEC;
 	printf("\nWriting took %g seconds..", seconds);
 
+	printf("\nReleasing memory..");
+	freeBars(start);
+
+	if(argc==4){
+		printf("\nDeleting random input file..");
+		if(remove(argv[1]))
+			printf("\nWarning : Unable to delete random input file!");
+	}
+
 	printf("\nCompleted successfully!\n");
+}
+
+
+void generateRandomAndTest(int count){
+	srand((unsigned int)time(NULL));
+	char buff[28];
+	time_t now = time(NULL);
+	strftime(buff, 28, "rndinpt_%Y%m%d_%H%M%S", localtime(&now));
+	FILE *f = fopen(buff, "w");
+	if(f==NULL){
+		printf("\nError : Unable to generate random input file!");
+		exit(1);
+	}
+	int i = 0;
+	while(i<count){
+		int ri = rand()%1000000;
+		double rf = ri + ((double)rand()/(double)RAND_MAX);
+		fprintf(f, "%g\n", rf);
+		i++;
+	}
+	fclose(f);
+	char* args[4];
+	args[0] = strdup("viscalc");
+	args[1] = strdup(buff);
+	args[2] = strdup("testoutput.txt");
+	args[3] = strdup("randomtest");
+	main(4, args);
 }
